0729f4cd24f62557d7f1bd1813777cd9cd75b41a
Leo <leoliu@apifox.com>
2023-04-18 09:57:07 +0800

    1

--- config.js
+++ config.js
@@ -2,9 +2,8 @@
 export const botName = 'gpt助手'
 
 // 群聊白名单，白名单内的群聊才会自动回复
-export const roomWhiteList = ['bot','一家亲']
+export const roomWhiteList = ['bot', '一家亲', '👦👦👧🏻🐶🐶', '打发🐶', '小胡向前冲']
 
 // 联系人白名单，白名单内的联系人才会自动回复
 
 export const aliasWhiteList = ['发狮', '心雨', '快骆肥宅水']
-
